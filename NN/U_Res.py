b82fd2fecaf70b447aed8929581d6696dd005882
Jacopo Altieri <jacopo.altieri@gmail.com>
2023-10-12 17:15:11 +0200

    Added dataset zipping and preprocessing

--- NN/U_Res.py
+++ NN/U_Res.py
@@ -29,10 +29,10 @@ from tensorflow.python.keras.optimizers import adam_v2
 from tensorflow.python.keras import Model
 
 
-batch_size = 50
+batch_size = 20
 
 # ================================================================#
-#               Importing dataset from directory                  #
+#              Importing dataset from directory                  #
 # ================================================================#
 input_path = r"C:\Users\jacop\Desktop\DL_Project\processed_dataset"
 
@@ -45,10 +45,13 @@ def process_image(x):
     return img
 
 
-train_images = tf.data.Dataset.list_files(input_path + "/images/train/*.jpg", shuffle=False)
+train_images = tf.data.Dataset.list_files(
+    input_path + "/images/train/*.jpg", shuffle=False
+)
 train_images = train_images.map(process_image)
-
-test_images = tf.data.Dataset.list_files(input_path + "/images/test/*.jpg", shuffle=False)
+test_images = tf.data.Dataset.list_files(
+    input_path + "/images/test/*.jpg", shuffle=False
+)
 test_images = test_images.map(process_image)
 
 
@@ -56,19 +59,34 @@ test_images = test_images.map(process_image)
 def load_labels(path):
     with open(path.numpy(), "r", encoding="utf-8") as file:
         lines = file.readlines()[1:]
-
     labels = [line.strip().split("\t") for line in lines]
     x_values = [float(label[1]) for label in labels]
     y_values = [float(label[2]) for label in labels]
-    landmarks = np.concatenate((x_values, y_values))
+    landmarks = np.concatenate((x_values, y_values))  # [x1,y1,x2,y2...,x14,y14]
     return landmarks
 
 
-train_labels = tf.data.Dataset.list_files(input_path + "/labels/train/*.txt", shuffle=False)
-train_labels = train_labels.map(lambda x: tf.py_function(load_labels, [x], [tf.float16]))
-
-test_labels = tf.data.Dataset.list_files(input_path + "/labels/test/*.txt", shuffle=False)
+train_labels = tf.data.Dataset.list_files(
+    input_path + "/labels/train/*.txt", shuffle=False
+)
+train_labels = train_labels.map(
+    lambda x: tf.py_function(load_labels, [x], [tf.float16])
+)
+test_labels = tf.data.Dataset.list_files(
+    input_path + "/labels/test/*.txt", shuffle=False
+)
 test_labels = test_labels.map(lambda x: tf.py_function(load_labels, [x], [tf.float16]))
+
+# =========== Combine Images and Labels ===========#
+train = tf.data.Dataset.zip((train_images, train_labels))
+train = train.shuffle(2000)
+train = train.batch(batch_size)
+train = train.prefetch(4)   # preload images to avoid bottlenecking
+
+test = tf.data.Dataset.zip((test_images, test_labels))
+test = test.shuffle(1000)
+test = test.batch(batch_size)
+test = test.prefetch(4)   # preload images to avoid bottlenecking
 
 
 
