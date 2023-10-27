e689daa5969146f92bca126c533dbe8673a82f44
Jacopo Altieri <jacopo.altieri@gmail.com>
2023-10-27 09:59:01 +0200

    Added comments

--- preprocessing_scripts/dataset_preprocessor.py
+++ preprocessing_scripts/dataset_preprocessor.py
@@ -4,7 +4,7 @@ AUTHORS: Altieri J. , Mazzini V.
 This module will rearrange the dataset in the correct format in order for it to be fed to the NN;
 The processed dataset will be stored in "processed_dataset" in your current working directory.
 It will find the matching image-labels pair from the original dataset and sort them into train, test and val folders based on a user-chosen percentage
-It will also automatically resize images and labels according to a user-chosen size
+It will also automatically resize images and labels according to a user-chosen size, and discard all the images smaller than a threshold
 
 You need to run this code only if working from the original dataset.
 """
@@ -16,26 +16,37 @@ import cv2
 import numpy as np
 import pandas as pd
 
-###### Functions #####
-def label_writer(path,coords):
+
+###### Functions ######
+
+def write_labels(path,coords):
     with open(path, 'w') as f:
         f.write("Landmark\tX\tY\n")
         for i, (x,y) in enumerate(coords, start=1):
             f.write(f"{i}\t{x}\t{y}\n")
 
+def count_lines(filename):
+    with open(filename, 'r') as file:
+        return sum(1 for line in file)
+
+
+###### Parameters ######
+
 MIN_SIZE = 300 # minimum required size
 NEW_SIZE = 256  # new image size
+
 # percentages of train,val and test dataset
 TRAIN_PERC = 0.60 
 VAL_PERC = 0.20
 TEST_PERC = 0.20
+
 
 input_path=input("Provide the path for the dataset: ")
 
 print("Preprocessing started...")
 
 # Creating the directory tree
-print("Creating the directory tree in the current wd...")
+print(f"Creating the directory tree in{os.getcwd()}")
 os.makedirs(os.getcwd() + "/processed_dataset/images/train", exist_ok=True)
 os.makedirs(os.getcwd() + "/processed_dataset/images/test", exist_ok=True)
 os.makedirs(os.getcwd() + "/processed_dataset/images/val", exist_ok=True)
@@ -52,19 +63,12 @@ for filename in os.listdir(input_path):
         old_filepath = os.path.join(input_path, filename)
         new_filepath = os.path.join(input_path, new_filename)
 
-# Define a function to count lines in a file
-def count_lines(filename):
-    with open(filename, 'r') as file:
-        return sum(1 for line in file)
+
     
 label_files = glob.glob("**/*[!README].txt", root_dir=input_path, recursive=True)
 label_files = [file for file in label_files if count_lines(input_path+"/"+file) == 15]  #removes files with more/less than 14 landmarks
 
-img_files = [
-    file
-    for file in glob.glob("**/*.jpg", root_dir=input_path, recursive=True)
-    if "-" not in file
-]
+img_files = [file for file in glob.glob("**/*.jpg", root_dir=input_path, recursive=True) if "-" not in file]
 
     
 # finding the matching img-label pairs
@@ -78,7 +82,7 @@ for i, j in enumerate(tqdm(matching)):
     im = cv2.imread(input_path+"/"+j+".jpg", cv2.IMREAD_UNCHANGED)
     original_size = im.shape[:2]
     if max(original_size) < MIN_SIZE:
-        continue  # Discard small images
+        continue  # Discard images smaller than threshold
         
     # converting grayscale images to rgb
     if len(im.shape) == 2:
@@ -104,10 +108,10 @@ for i, j in enumerate(tqdm(matching)):
 
 
     if i<int(len(matching)*TRAIN_PERC):        
-        label_writer(os.getcwd()+"/processed_dataset/labels/train/"+str(i)+".txt", new_coords) 
+        write_labels(os.getcwd()+"/processed_dataset/labels/train/"+str(i)+".txt", new_coords) 
     elif i < int(len(matching) * (TRAIN_PERC + VAL_PERC)):
-        label_writer(os.getcwd()+"/processed_dataset/labels/val/"+str(i)+".txt", new_coords) 
+        write_labels(os.getcwd()+"/processed_dataset/labels/val/"+str(i)+".txt", new_coords) 
     else:
-        label_writer(os.getcwd()+"/processed_dataset/labels/test/"+str(i)+".txt", new_coords)
+        write_labels(os.getcwd()+"/processed_dataset/labels/test/"+str(i)+".txt", new_coords)
         
 print('Preprocessing complete! The dataset in "'+os.getcwd()+'/processed_dataset" is ready to be used!')
